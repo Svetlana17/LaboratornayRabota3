5ca489d4cd0b10a36d26504f4d86bf7df9ecd77f
Users <lana.chebotareva.95@mail.ru>
2018-11-28 19:19:25 +0300

    class serilazable

--- command
+++ command
@@ -39,3 +39,11 @@ English
 ___________
 Ivanova  5 5 0 0 0  Geo English Biology null null
 ___________
+___________
+Ivanova
+English
+5 
+
+___________
+Ivanova  5 5 0 0 0  Geo English Biology null null
+___________
--- src/main/java/Memento/SerilazeStudent.java
+++ src/main/java/Memento/SerilazeStudent.java
@@ -1,4 +1,21 @@
 package Memento;
 
-public class SerilazeStudent {
+import Command.Student;
+
+import java.io.*;
+
+public class SerilazeStudent  {
+    public static  void main(String args[]) throws IOException, ClassNotFoundException {
+    ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+    ObjectOutput objectOutput=new ObjectOutputStream(byteArrayOutputStream);
+
+    Student student=new Student("petrov" , 2);
+//
+    objectOutput.writeObject(student);
+    objectOutput.flush();
+    ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream("student.dat"));
+    Student student1= (Student) objectInputStream.readObject();
+    student1.toString();
+    System.out.println(student1.toString());
+}
 }
