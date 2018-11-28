d233f346c52cf0144cdea93c0bacdea0bb5267f5
Users <lana.chebotareva.95@mail.ru>
2018-11-28 20:57:31 +0300

    class serilazable

--- src/main/java/Memento/SerilazeStudent.java
+++ src/main/java/Memento/SerilazeStudent.java
@@ -10,12 +10,12 @@ public class SerilazeStudent  {
     ObjectOutput objectOutput=new ObjectOutputStream(byteArrayOutputStream);
 
     Student student=new Student("petrov" , 2);
-//
     objectOutput.writeObject(student);
     objectOutput.flush();
-    ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream("student.dat"));
-    Student student1= (Student) objectInputStream.readObject();
-    student1.toString();
-    System.out.println(student1.toString());
+    System.out.println("Данные записаны");
+//    ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream("student.dat"));
+//  Student student1= (Student) objectInputStream.readObject();
+ //   student1.toString();
+//   System.out.println(student1.toString());
 }
 }
--- src/main/java/PupilMain.java
+++ src/main/java/PupilMain.java
@@ -8,10 +8,12 @@ public class PupilMain {
         Pupil student =new Student("Petrov" ,4);
         student.setSubjectAt(0,"Geo");
         student.setSubjectAt(1, "English");
-        student.setSubjectAt(2, "Biology");
+//        student.setSubjectAt(2, "Biology");
+//        student.setSubjectAt(3, "OOP");
         student.setMarkAt(0,5);
         student.setMarkAt(1,3);
-        student.setMarkAt(2,3);
+//        student.setMarkAt(2,3);
+//        student.setMarkAt(3,3);
         PupilChain col = new PrintPupilCol();
         PupilChain row = new PrintPupilRow();
         col.setNextChain(row);
